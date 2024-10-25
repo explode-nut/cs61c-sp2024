aa40b533e57176b3a2e5b85a932c9ffd1268dd34
explode-nut <1072215361@qq.com>
2024-10-25 17:23:56 +0800

    proj3 task5 自定义测试通过

--- project/proj3/tests/integration-custom/in/test_custom.s
+++ project/proj3/tests/integration-custom/in/test_custom.s
@@ -1,5 +1,26 @@
 # test i type instruction
-addi t1, x0, -2
-srai t2, t1, 1
-addi t0, x0, -3
-addi t0, x0, 10
+# addi test
+addi t0, x0, 3
+addi t0, x0, -2
+# srai test
+srai t1, t0, 1
+addi t0, x0, 5
+srai t1, t0, 2
+# andi ori xori test
+addi t0, x0, 27
+andi t1, t0, 3
+ori t1, t0, 4
+xori t1, t0, 5
+# slli test
+slli t1, t0, 3
+# srli test
+srli t1, t0, 2
+addi t0, x0, -7
+srli t1, t0, 3
+# slti test
+slti t1, t0, 3
+slti t1, t0, -3
+slti t1, t0, -9
+addi t0, x0, 9
+slti t1, t0, 3
+slti t1, t0, 12
