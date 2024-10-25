15799995a02cea67ab7082fdc5a0df850a5b1f4a
explode-nut <1072215361@qq.com>
2024-10-25 21:16:00 +0800

    proj3 task6完成 自定义测试通过

--- project/proj3/tests/integration-custom/in/test_custom.s
+++ project/proj3/tests/integration-custom/in/test_custom.s
@@ -24,3 +24,51 @@ slti t1, t0, -9
 addi t0, x0, 9
 slti t1, t0, 3
 slti t1, t0, 12
+
+# test r type instruction
+# add test
+addi s0, x0, -13
+addi s1, x0, 2043
+addi s2, x0, 23
+addi t0, x0, -15
+addi t2, x0, 3
+add t1, t0, t2
+addi t0, x0, 15
+addi t2, x0, 3
+add t1, t0, t2
+# sra test
+sra t1, t0, t2
+sra t1, s0, t2
+# and or xor test
+and t1, t0, t2
+or t1, t0, t2
+xor t1, t0, t2
+# sll test
+sll t1, t0, t2
+# srl test
+srl t1, t0, t2
+srl t1, s0, t2
+# slt test
+slt t1, t0, t2
+slt t1, t2, t0
+slt t1, s0, t0
+slt t1, t0, s0
+# sub test
+sub t1, t2, t0
+sub t1, s0, t2
+sub t1, t0, s0
+# mul test
+mul t1, t0, t2
+mul t1, t0, s0
+mul t1, s1, s2
+mul t1, s1, s0
+# mulh test
+mul t1, t0, t2
+mul t1, t0, s0
+mul t1, s1, s2
+mul t1, s1, s0
+# mulhu test
+mul t1, t0, t2
+mul t1, t0, s0
+mul t1, s1, s2
+mul t1, s1, s0
